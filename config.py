15cfa52bca26014a18b20c58865092b81ad375ba
rogerioponsoni <rogerio.ponsoni@gmail.com>
2025-02-06 11:48:15 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
 API_BASE_URL = "https://protectmeseguros.digisac.biz/api/v1/messages"  # Substitua pela URL base da API
-API_TOKEN = "Bearer 724c60d95d8911ab00097c497f459099233641c5"       # Substitua pelo seu token de autenticação
+API_TOKEN = "724c60d95d8911ab00097c497f459099233641c5"       # Substitua pelo seu token de autenticação
