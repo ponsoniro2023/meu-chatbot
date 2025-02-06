ce3e8fbfdf594123806ae3aeb9e4a6fc935d694f
rogerioponsoni <rogerio.ponsoni@gmail.com>
2025-02-06 13:41:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ def enviar_mensagem(numero_telefone: str, mensagem: str):
         "type": "chat",
         "text": mensagem,
         "serviceId": "8e473787-7548-417f-83e1-5eb1bd533d6f",
-        "userId" :"d2787b46-36fd-4718-93f7-1c86f0e3cab9",
+        "userId": "d2787b46-36fd-4718-93f7-1c86f0e3cab9",
         "dontOpenTicket": True
-    
     }
     response = requests.post(url, json=payload, headers=headers)
     
